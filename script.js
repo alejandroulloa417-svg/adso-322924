559aef0c70f785356b5955ae1ba40a1a2911974e
alejandroulloa <alejandroulloa417@gmail.com>
2026-02-24 12:08:24 -0500

    modificado desde rama4

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function mostrarDia() {
     let numero = parseInt(document.getElementById("numeroDia").value);
     let dia;
 
-    switch (numero) {
+    switch (numero3) {
         case 1:
             dia = "Lunes";
             break;
